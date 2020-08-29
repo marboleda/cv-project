a2046b36180d17027c55516cb4ccd2b72289fed3
Marco Arboleda <arboleda.marco@gmail.com>
2020-08-28 22:18:04 -0400

    Implement submit/edit functionality of General Info

--- src/App.js
+++ src/App.js
@@ -7,12 +7,67 @@ import Education from './components/Education'
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      editingGeneralInfo: true,
+      generalInfoName: "",
+      generalInfoEmail: "",
+      generalInfoPhoneNumber: "",
+    }
+
+    this.generalInfoNameChangeHandler = this.generalInfoNameChangeHandler.bind(this);
+    this.generalInfoEmailChangeHandler = this.generalInfoEmailChangeHandler.bind(this);
+    this.generalInfoPhoneNumberChangeHandler = this.generalInfoPhoneNumberChangeHandler.bind(this);
+    this.generalInfoSubmitHandler = this.generalInfoSubmitHandler.bind(this);
+    this.generalInfoEditButtonHandler = this.generalInfoEditButtonHandler.bind(this);
+  }
+
+  generalInfoNameChangeHandler = (event) => {
+    this.setState({
+      generalInfoName: event.target.value
+    });
+  }
+
+  generalInfoEmailChangeHandler = (event) => {
+    this.setState({
+      generalInfoEmail: event.target.value
+    });
+  }
+
+  generalInfoPhoneNumberChangeHandler = (event) => {
+    this.setState({
+      generalInfoPhoneNumber: event.target.value
+    })
+  }
+
+  generalInfoSubmitHandler = () => {
+    this.setState({
+      editingGeneralInfo: false,
+    })
+  }
+
+  generalInfoEditButtonHandler = () => {
+    this.setState({
+      editingGeneralInfo: true
+    });
   }
 
   render() {
+    const { editingGeneralInfo, generalInfoName, generalInfoEmail, generalInfoPhoneNumber } = this.state;
+
     return (
       <div className="App">
-        <GeneralInfo />
+        <GeneralInfo 
+          submit={this.generalInfoSubmitHandler} 
+          edit={this.generalInfoEditButtonHandler}
+          isEditing={editingGeneralInfo} 
+          name={generalInfoName}
+          email={generalInfoEmail}
+          phone={generalInfoPhoneNumber}
+          nameChange={this.generalInfoNameChangeHandler}
+          phoneChange={this.generalInfoPhoneNumberChangeHandler}
+          emailChange={this.generalInfoEmailChangeHandler}
+        />
         <WorkExperience />
         <Education />
       </div>
--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,29 +1,45 @@
 import React from 'react';
 
 const generalInfo = (props) => {
-    return(
-        <div className="generalInfo">
-            <form name="general-info">
-                <h2>General Info</h2>
+    if (props.isEditing) {
+        return(
+            <div className="generalInfo">
+                <form 
+                    onSubmit={props.submit} 
+                    name="general-info"
+                >
+                
+                    <h2>General Info</h2>
 
-                <label for="name">
-                    <strong>Name:</strong>
-                </label>
-                <input type="text" name="name" required />
+                    <label for="name">
+                        <strong>Name:</strong>
+                    </label>
+                    <input type="text" name="name" onChange={props.nameChange} required />
 
-                <label for="email">
-                    <strong>Email:</strong>
-                </label>
-                <input type="text" name="email" />
+                    <label for="email">
+                        <strong>Email:</strong>
+                    </label>
+                    <input type="text" name="email" onChange={props.emailChange} />
 
-                <label for="phone">
-                    <strong>Phone Number:</strong>
-                </label>
+                    <label for="phone">
+                        <strong>Phone Number:</strong>
+                    </label>
+                    <input type="text" name="phone" onChange={props.phoneChange} />
 
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    } else {
+        return(
+            <div>
+                <h1>{props.name}</h1>
+                <p>{props.email}</p>
+                <p>{props.phone}</p>
+                <button id="edit-general-info-button" onClick={props.edit}>Edit</button>
+            </div>
+        )
+    }
 }
 
 export default generalInfo
