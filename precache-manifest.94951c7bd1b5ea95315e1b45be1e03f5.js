852fc7b1a54c223c1c19d40cd8682f9c7c46ec52
Marco Arboleda <arboleda.marco@gmail.com>
2020-09-27 20:53:56 -0400

    Updates

---
+++ precache-manifest.94951c7bd1b5ea95315e1b45be1e03f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8670f3713ebfe450c42c52dcc558eb2c",
+    "url": "/marboleda/cv-project/index.html"
+  },
+  {
+    "revision": "5dc1140f088275d36225",
+    "url": "/marboleda/cv-project/static/css/main.4037c3ab.chunk.css"
+  },
+  {
+    "revision": "500b16d0b0af07501aa0",
+    "url": "/marboleda/cv-project/static/js/2.1e1cfc50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marboleda/cv-project/static/js/2.1e1cfc50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc1140f088275d36225",
+    "url": "/marboleda/cv-project/static/js/main.de2946a9.chunk.js"
+  },
+  {
+    "revision": "4e399e47507e1bf863cf",
+    "url": "/marboleda/cv-project/static/js/runtime-main.ee0fcbaf.js"
+  }
+]);
